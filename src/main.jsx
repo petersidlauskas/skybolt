bb3bcb633b32220193b192f4090cc8bdc296c6ed
Peter Sidlauskas <petersidlauskas@Peters-iMac-2.local>
2023-04-18 18:39:26 -0400

    future project, adding files

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+   <div class="nav"> 
+     <ul>
+       <li>Thanks for watching...</li>
+     </ul>
+     </div>
+    <App  />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
