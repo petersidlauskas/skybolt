4e38779750fc3018de7a8c44c6f1091484ea5881
Peter Sidlauskas <peter@bronze56k.com>
2023-04-19 20:41:09 -0400

    added navbar

---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+<div class="nav"> 
+     <ul>
+       <li>Shop</li>
+       <li>Video</li>
+       <li>Radio</li>
+       <li>Stockists</li>
+     </ul>
+     </div>
+  );
+
+};
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Nav from './Nav'
 
 ReactDOM.render(
   <React.StrictMode>
-   <div class="nav"> 
-     <ul>
-       <li>Thanks for watching...</li>
-     </ul>
-     </div>
+   <Nav />
     <App  />
   </React.StrictMode>,
   document.getElementById('root')
