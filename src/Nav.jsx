5dfca79ab95bab3993fd74dd192bd5cdbff5d9e7
Peter Sidlauskas <peter@bronze56k.com>
2023-04-19 20:53:05 -0400

    added a tag

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
 <div class="nav"> 
      <ul>
-       <li>Shop</li>
+       <li><a href="http://shop.bronze56k.com">Shop</a></li>
        <li>Video</li>
        <li>Radio</li>
        <li>Stockists</li>
